a6e3b9ad0b9396eac808416858b812056007c093
asadeyy <asada.kanna.819@s.kyushu-u.ac.jp>
2023-02-13 11:47:02 +0900

    confirm python version

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 # exit on error
 set -o errexit
 
-pip install --upgrade pip
+python --version
+pip3 install --upgrade pip
 pip3 install -r requirements.txt
 
 python3 manage.py collectstatic --no-input
